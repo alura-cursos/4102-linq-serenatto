4045a1ffdba5cdcb7e14c230374165dd68350caf
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-09-23 15:58:33 -0300

    video 1.2

--- Serenatto/Program.cs
+++ Serenatto/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using SerenattoEnsaio.Dados;
+using SerenattoEnsaio.Modelos;
+
+IEnumerable<Cliente> clientes = DadosClientes.GetClientes().ToList();
+
+Console.WriteLine("RELATÓRIO DE DADOS CLIENTES");
+foreach (var cliente in clientes)
+{
+    Console.WriteLine($"{cliente.Id} | {cliente.Nome} | {cliente.Endereco} | {cliente.Telefone}");
+}
+
