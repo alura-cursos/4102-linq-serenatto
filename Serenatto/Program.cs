ff4e7ae3f7031ebacea55d452e71f32d37d9060a
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-09-24 11:50:00 -0300

    video 2.1

--- Serenatto/Program.cs
+++ Serenatto/Program.cs
@@ -1,8 +1,10 @@
 ﻿using SerenattoEnsaio.Dados;
 using SerenattoEnsaio.Modelos;
+using SerenattoPreGravacao.Dados;
 
 IEnumerable<Cliente> clientes = DadosClientes.GetClientes().ToList();
 IEnumerable<string> formasPagamento = DadosFormaDePagamento.FormasDePagamento;
+IEnumerable<Produto> cardapioLoja = DadosCardapio.GetProdutos();
 
 Console.WriteLine("RELATÓRIO DE DADOS CLIENTES");
 foreach (var cliente in clientes)
@@ -21,5 +23,25 @@ Console.WriteLine(string.Join(" ", pesquisa));
 var pesquisa2 = formasPagamento.Where(p => p.StartsWith('d'));
 
 Console.WriteLine(string.Join(" ", pesquisa2));
-              
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("RELATÓRIO DADOS CARDÁPIO LOJA");
+
+cardapioLoja.ToList();
+
+foreach (var item in cardapioLoja)
+{
+    Console.WriteLine($"{item.Id} | {item.Nome} | {item.Descricao} | {item.Preco}");
+}
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("RELATÓRIO PRODUTOS DO CARDÁPIO POR NOME");
+
+var produtosPorNome = cardapioLoja.Select(p => p.Nome);
+
+foreach (var item in produtosPorNome)
+{
+    Console.WriteLine(item);
+}
+
 
